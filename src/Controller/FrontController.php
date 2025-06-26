7702f9428cb725336bb6fd0656aa3cd6f84d5379
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-06-26 17:37:00 +0200

    Add tailwind and flowbite, create about page

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,11 +8,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class FrontController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function home(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('front/index.html.twig');
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('front/about.html.twig');
     }
 }
